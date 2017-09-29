2a7e07bd298ef9c07ec84c26a14b23ba22617526
jhsware <sebastian@urbantalk.se>
2017-09-29 18:03:19 +0200

    Remove some attributes to clean up dom

--- lib/Animated.jsx
+++ lib/Animated.jsx
@@ -3,6 +3,8 @@ import Component from 'inferno-component'
 import createElement from 'inferno-create-element'
 import { animateOnAdd, animateOnRemove } from './animatedComponent'
 
+const excludeProps = {'el': true, 'tag': true, 'prefix': true, children: true}
+
 class Animated extends Component {
 
   componentDidMount () {
@@ -14,10 +16,18 @@ class Animated extends Component {
   }
 
   render () {
+    const props = {}
+    const keys = Object.keys(this.props)
+    for (let i = 0; i < keys.length; i++) {
+      let tmpKey = keys[i]
+      if (!excludeProps[tmpKey]) {
+        props[tmpKey] = this.props[tmpKey]
+      }
+    }
+
     return createElement(
-      this.props.el || 'div', 
-      this.props, 
       this.props.el || this.props.tag || 'div', 
+      props, 
       this.props.children
     )
   }
