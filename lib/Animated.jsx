c579b6baf4258e9e70442e1f3043fc0ad0c77d8f
jhsware <sebastian@urbantalk.se>
2018-03-02 20:13:10 +0100

    Moved bootstrap specific animations to  inferno-bootstrap

--- lib/Animated.jsx
+++ lib/Animated.jsx
@@ -2,7 +2,6 @@
 import { Component } from 'inferno'
 import { createElement } from 'inferno-create-element'
 import { animateOnAdd, animateOnRemove } from './animatedComponent'
-import { animateBootstrapOnAdd, animateBootstrapOnRemove } from './animatedBootstrap'
 
 const excludeProps = {
   el: true,
@@ -31,15 +30,6 @@ function OriginalAnimated (props) {
 }
 
 
-function BootstrapAnimated (props) {
-  const attr = {}
-  const keys = Object.keys(props)
-  for (let i = 0; i < keys.length; i++) {
-    let tmpKey = keys[i]
-    if (!excludeProps[tmpKey]) {
-      attr[tmpKey] = props[tmpKey]
-    }
-  }
 
   return createElement(
     props.el || props.tag || 'div', 
@@ -50,14 +40,6 @@ function BootstrapAnimated (props) {
 
 function Animated (props) {
   if (props.target === 'bootstrap') {
-    const animCls = {
-      start: props.prefix.hide, // 'collapse'
-      active: props.prefix.active, // 'collapsing'
-      end: props.prefix.show // 'collapse show'
-    }
-    return <BootstrapAnimated {...props}
-      onComponentDidMount={(dom) => animateBootstrapOnAdd(dom, animCls, props.onDidEnter)}
-      onComponentWillUnmount={(dom) => animateBootstrapOnRemove(dom, animCls, props.onDidLeave)} />
   } else {
     return <OriginalAnimated {...props}
       onComponentDidMount={(dom) => animateOnAdd(dom, props.prefix, props.onDidEnter)}
