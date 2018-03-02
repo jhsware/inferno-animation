4ca02692bbdd113beb2b547652ef72d093487ce6
jhsware <sebastian@urbantalk.se>
2018-03-02 20:14:43 +0100

    Simplification

--- lib/Animated.jsx
+++ lib/Animated.jsx
@@ -3,48 +3,44 @@ import { Component } from 'inferno'
 import { createElement } from 'inferno-create-element'
 import { animateOnAdd, animateOnRemove } from './animatedComponent'
 
-const excludeProps = {
-  el: true,
-  tag: true,
-  prefix: true,
-  bootstrapCls: true,
-  prefixLeave: true,
-  children: true
-}
-
-function OriginalAnimated (props) {
-  const attr = {}
-  const keys = Object.keys(props)
-  for (let i = 0; i < keys.length; i++) {
-    let tmpKey = keys[i]
-    if (!excludeProps[tmpKey]) {
-      attr[tmpKey] = props[tmpKey]
-    }
-  }
-
+function OriginalAnimated ({ el, tag, children, ...attrs}) {
   return createElement(
-    props.el || props.tag || 'div', 
-    attr, 
-    props.children
+    tag || el || 'div', 
+    attrs, 
+    children
   )
 }
 
+function Animated ({ prefix, onDidEnter, onDidLeave, ...attrs}) {
 
-
-  return createElement(
-    props.el || props.tag || 'div', 
-    attr, 
-    props.children
-  )
-}
-
-function Animated (props) {
-  if (props.target === 'bootstrap') {
-  } else {
-    return <OriginalAnimated {...props}
-      onComponentDidMount={(dom) => animateOnAdd(dom, props.prefix, props.onDidEnter)}
-      onComponentWillUnmount={(dom) => animateOnRemove(dom, props.prefix, props.onDidLeave)} />
-  }
+  return <OriginalAnimated {...attrs}
+    onComponentDidMount={(dom) => animateOnAdd(dom, prefix, onDidEnter)}
+    onComponentWillUnmount={(dom) => animateOnRemove(dom, prefix, onDidLeave)} />
 }
 
 export default Animated
+
+/*
+
+// Why did this not work?
+
+'use strict'
+import { Component } from 'inferno'
+import { createElement } from 'inferno-create-element'
+import { animateOnAdd, animateOnRemove } from './animatedComponent'
+
+function Animated ({ el, tag, prefix, onDidEnter, onDidLeave, children, ...attrs}) {
+
+  attrs.onComponentDidMount = (dom) => animateOnAdd(dom, prefix, onDidEnter)
+  attrs.onComponentWillUnmount = (dom) => animateOnRemove(dom, prefix, onDidLeave)
+
+  return createElement(
+    tag || el || 'div', 
+    attrs, 
+    children
+  )
+}
+
+export default Animated
+
+*/
