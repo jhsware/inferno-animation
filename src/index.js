1ab6d607bfc2efd0e1efc260c1f94394c5562f65
jhsware <sebastian@urbantalk.se>
2018-07-02 09:21:58 +0200

    Trying a different way to export to see if it solves an issue with imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-export { default as Animated } from './Animated';
-export { default as CrossFade } from './CrossFade';
-export { animateOnRemove, animateOnAdd } from './animatedComponent';
-export * from './utils';
+export { default as Animated } from './Animated'
+export { default as CrossFade } from './CrossFade'
+export { animateOnRemove, animateOnAdd } from './animatedComponent'
+import {
+  addClassName,
+  animationIsRunningOnParent,
+  removeClassName,
+  registerTransitionListener,
+  forceReflow,
+  clearDimensions,
+  getDimensions,
+  setDimensions,
+  setDisplay
+} from './utils'
+
+export const utils = {
+  addClassName,
+  animationIsRunningOnParent,
+  removeClassName,
+  registerTransitionListener,
+  forceReflow,
+  clearDimensions,
+  getDimensions,
+  setDimensions,
+  setDisplay
+}
