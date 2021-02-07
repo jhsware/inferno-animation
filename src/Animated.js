70c2e1ce842cdde9031ce4d74f957c8092067e04
jhsware <sebastian@urbantalk.se>
2021-02-07 19:25:16 +0100

    Fixed an issue so we can get the dom ref from the animated component. ref={} won't be passed through createComponentVnode

--- src/Animated.js
+++ src/Animated.js
@@ -1,17 +1,20 @@
 import { createElement } from 'inferno-create-element'
 import { animateOnAdd, animateOnRemove } from './animatedComponent'
 
-function OriginalAnimated ({ el, tag, children, ...attrs}) {
+function OriginalAnimated ({ domRef, el, tag, children, ...attrs}) {
+
   return createElement(
-    tag || el || 'div', 
-    attrs, 
+    tag || el || 'div',
+    {ref: domRef, ...attrs}, 
     children
   )
 }
 
-function Animated ({ prefix, onDidEnter, onDidLeave, ...attrs}) {
-
+function Animated ({ domRef, prefix, onDidEnter, onDidLeave, ...attrs}) {
+  // I can't pass the ref callback through the ref attr so using domRef as
+  // a work around.
   return <OriginalAnimated {...attrs}
+    domRef={domRef}
     onComponentDidMount={(dom) => animateOnAdd(dom, prefix, onDidEnter)}
     onComponentWillUnmount={(dom) => animateOnRemove(dom, prefix, onDidLeave)} />
 }
